276527582b78fdabcdcf95f49a061257f00405dc
whysaint <taras.panchuk.un@gmail.com>
2025-10-26 20:42:06 +0100

    Add animation

--- Assets/Scripts/PlatformerPlayer.cs
+++ Assets/Scripts/PlatformerPlayer.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using UnityEngine;
 
 public class PlatformerPlayer : MonoBehaviour
@@ -6,10 +7,12 @@ public class PlatformerPlayer : MonoBehaviour
     public float speed = 4.5f;
 
     private Rigidbody2D _rb;
-
+    private Animator _anim;
+    
     private void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
+        _anim = GetComponent<Animator>();
     }
 
     private void Update()
@@ -17,5 +20,11 @@ public class PlatformerPlayer : MonoBehaviour
         float deltaX = Input.GetAxis("Horizontal") * speed;
         Vector2 movement = new Vector2(deltaX, _rb.linearVelocity.y);
         _rb.linearVelocity = movement;
+        
+        _anim.SetFloat("speed", Mathf.Abs(deltaX));
+        if (!Mathf.Approximately(deltaX, 0))
+        {
+            transform.localScale = new Vector3(Mathf.Sign(deltaX), 1, 1);
+        }
     }
 }

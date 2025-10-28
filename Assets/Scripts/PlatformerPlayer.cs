51f2864d547357e0769270d6ca75f7635c3ca1d2
whysaint <taras.panchuk.un@gmail.com>
2025-10-28 12:40:35 +0100

    Add jump player

--- Assets/Scripts/PlatformerPlayer.cs
+++ Assets/Scripts/PlatformerPlayer.cs
@@ -5,14 +5,17 @@ using UnityEngine;
 public class PlatformerPlayer : MonoBehaviour
 {
     public float speed = 4.5f;
+    public float jumpForce = 12.0f;
 
     private Rigidbody2D _rb;
     private Animator _anim;
+    private BoxCollider2D _box;
     
     private void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
         _anim = GetComponent<Animator>();
+        _box = GetComponent<BoxCollider2D>();
     }
 
     private void Update()
@@ -20,6 +23,18 @@ public class PlatformerPlayer : MonoBehaviour
         float deltaX = Input.GetAxis("Horizontal") * speed;
         Vector2 movement = new Vector2(deltaX, _rb.linearVelocity.y);
         _rb.linearVelocity = movement;
+
+        Vector3 max = _box.bounds.max;
+        Vector3 min = _box.bounds.min;
+        Vector2 corner1 = new Vector2(max.x, min.y - .1f);
+        Vector2 corner2 = new Vector2(min.x, min.y - .2f);
+        Collider2D hit = Physics2D.OverlapArea(corner1, corner2);
+
+        bool grounded = hit != null;
+        if (grounded && Input.GetKeyDown(KeyCode.Space))
+        {
+            _rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
         
         _anim.SetFloat("speed", Mathf.Abs(deltaX));
         if (!Mathf.Approximately(deltaX, 0))

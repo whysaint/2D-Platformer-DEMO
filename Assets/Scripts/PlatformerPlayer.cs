b6577e999fa6a38b894e98f99b6240256328d11e
whysaint <taras.panchuk.un@gmail.com>
2025-10-30 11:47:41 +0100

    Finish

--- Assets/Scripts/PlatformerPlayer.cs
+++ Assets/Scripts/PlatformerPlayer.cs
@@ -8,9 +8,7 @@ public class PlatformerPlayer : MonoBehaviour
     private Rigidbody2D _rb;
     private Animator _anim;
     private BoxCollider2D _box;
-    private float _moveInput;
-    private bool _jumpPressed;
-
+   
     private void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
@@ -20,15 +18,7 @@ public class PlatformerPlayer : MonoBehaviour
 
     private void Update()
     {
-        _moveInput = Input.GetAxis("Horizontal");
-
-        if (Input.GetKeyDown(KeyCode.Space))
-            _jumpPressed = true;
-    }
-
-    private void FixedUpdate()
-    {
-        float deltaX = _moveInput * speed;
+        float deltaX = Input.GetAxis("Horizontal") * speed;
         Vector2 movement = new Vector2(deltaX, _rb.linearVelocity.y);
         _rb.linearVelocity = movement;
 
@@ -36,18 +26,40 @@ public class PlatformerPlayer : MonoBehaviour
         Vector3 min = _box.bounds.min;
         Vector2 corner1 = new Vector2(max.x, min.y - .1f);
         Vector2 corner2 = new Vector2(min.x, min.y - .2f);
-        bool grounded = Physics2D.OverlapArea(corner1, corner2);
+        Collider2D hit = Physics2D.OverlapArea(corner1, corner2);
 
-        if (grounded && _jumpPressed)
+        bool grounded = hit != null;
+        if (grounded && Input.GetKeyDown(KeyCode.Space))
         {
             _rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
         }
-        _jumpPressed = false;
 
+        MovingPlatform platform = null;
+        if (hit != null)
+        {
+            platform = hit.GetComponent<MovingPlatform>();
+        }
+
+        if (platform != null)
+        {
+            transform.parent = platform.transform;
+        }
+        else
+        {
+            transform.parent = null;
+        }
+       
         _anim.SetFloat("speed", Mathf.Abs(deltaX));
+
+        Vector3 pScale = Vector3.one;
+        if (platform != null)
+        {
+            pScale = platform.transform.localScale;
+        }
+
         if (!Mathf.Approximately(deltaX, 0))
         {
-            transform.localScale = new Vector3(Mathf.Sign(deltaX), 1, 1);
+            transform.localScale = new Vector3(Mathf.Sign(deltaX) /pScale.x, 1/pScale.y, 1);
         }
     }
 }
